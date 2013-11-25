b2a88ec5d36d8d5ee1d0e4b92a8b60e0a9bfbe88
ryo sakuma <ryo@hikari.fam.cx>
2013-11-25 12:19:39 +0900

    Incomplete - task : dbunit tab to space

--- src/hikari/test/DbTestCase1.java
+++ src/hikari/test/DbTestCase1.java
@@ -17,40 +17,35 @@ import org.junit.Test;
  */
 public class DbTestCase1 extends DBTestCase {
 
-	public DbTestCase1(String name) {
-		super(name);
-		System.setProperty(
-				PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,
-				"org.hsqldb.jdbcDriver");
-		System.setProperty(
-				PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
-				"jdbc:hsqldb:sample");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME,
-				"sa");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD,
-				"");
-		// System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA,
-		// "" );
-	}
+    public DbTestCase1(String name) {
+        super(name);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,
+                "org.hsqldb.jdbcDriver");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
+                "jdbc:hsqldb:sample");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "sa");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "");
+        // System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA,
+        // "" );
+    }
 
-	@Override
-	protected IDataSet getDataSet() throws Exception {
-		return new FlatXmlDataSetBuilder().build(new FileInputStream(
-				"dataset.xml"));
-	}
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(new FileInputStream("dataset.xml"));
+    }
 
-	@Override
-	protected DatabaseOperation getSetUpOperation() throws Exception {
-		return DatabaseOperation.REFRESH;
-	}
+    @Override
+    protected DatabaseOperation getSetUpOperation() throws Exception {
+        return DatabaseOperation.REFRESH;
+    }
 
-	@Override
-	protected DatabaseOperation getTearDownOperation() throws Exception {
-		return DatabaseOperation.NONE;
-	}
+    @Override
+    protected DatabaseOperation getTearDownOperation() throws Exception {
+        return DatabaseOperation.NONE;
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
 }
