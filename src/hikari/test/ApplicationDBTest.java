8ddb9c960ddfa2876f411505299001aeff628cd1
sakuma <sakuma@ebisu>
2014-03-24 16:08:46 +0900

    新規ファイル

---
+++ src/hikari/test/ApplicationDBTest.java
@@ -0,0 +1,105 @@
+package hikari.test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.excel.XlsDataSet;
+import org.dbunit.dataset.filter.DefaultColumnFilter;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApplicationDBTest {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationDBTest.class);
+
+    IDatabaseConnection connection;
+
+    @Before
+    public void setUp() throws ClassNotFoundException, SQLException, DatabaseUnitException,
+            IOException {
+        connection = DBConnection.from(DBConfig.fromProperty("/serverapplication.properties", this.getClass()));
+    }
+    
+    @After
+    public void down() throws SQLException{
+        connection.close();
+    }
+
+    @Test
+    public void testSave() throws DataSetException, FileNotFoundException, IOException{
+        QueryDataSet ds = new QueryDataSet(connection);
+        ds.addTable("inter_basic");
+        XlsDataSet.write(ds, new FileOutputStream("ap.xls"));
+        FlatXmlDataSet.write(ds, new FileOutputStream("ap.xml"));
+    }
+//    
+//    @Test
+//    public void testLoadXml() throws DatabaseUnitException, Exception {
+//        logger.info("testLoadXml開始");
+//
+//        IDataSet ds = DataSet.loadXml("/data2.xml", "/dtd.xml", getClass());
+//
+//        XlsDataSetWriter writer = new XlsDataSetWriter();
+//        writer.write(ds, new FileOutputStream("target/out.xls"));
+//
+//        try {
+//            DatabaseOperation.CLEAN_INSERT.execute(connection, ds);
+//        } finally {
+//            connection.close();
+//        }
+//    }
+//
+//    @Test
+//    public void testLoadXls() throws DatabaseUnitException, Exception {
+//    
+//        logger.info("testLoadXls開始");
+//    
+//        IDataSet ds = DataSet.loadXls("/data1.xls");
+//    
+//        XlsDataSetWriter writer = new XlsDataSetWriter();
+//        writer.write(ds, new FileOutputStream("target/out3.xls"));
+//    
+//        try {
+//            // データベースにあってデータセットにないテーブルは影響を受けない
+//            DatabaseOperation.CLEAN_INSERT.execute(connection, ds);
+//        } finally {
+//            connection.close();
+//        }
+//    }
+
+    // @Test
+    public void testAssertXml() throws DatabaseUnitException, Exception {
+        logger.info("開始");
+
+        IDataSet ds = DataSet.loadXml("/data2.xml", "/dtd.xml", getClass());
+        IDataSet ds2 = DataSet.loadXml("/hikari/test/data3.xml", "/dtd.xml", getClass());
+        // IDataSet ds2 = DataSet.loadXml("/hikari/test/data3.xml", "/dtd.xml",
+        // getClass());
+
+        Assertion.assertEquals(ds, ds2);
+    }
+
+    //@Test
+    public void testIgnoreColumns() throws DatabaseUnitException {
+        ITable actual = null;
+        ITable expected = null;
+
+        ITable filteredTable = DefaultColumnFilter.includedColumnsTable(actual, expected
+                .getTableMetaData().getColumns());
+      
+        Assertion.assertEquals(expected, filteredTable);
+    }
+
+}
