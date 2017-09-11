3a8194e39639e5bee948347bc96056c96035d627
佐久間良 <ryo-sakuma@nii.co.jp>
2017-09-11 12:08:00 +0900

    設定用クラス修正

--- src/test/java/hikari/dbunit/AbstractSpringDbConfig.java
+++ src/test/java/hikari/dbunit/AbstractSpringDbConfig.java
@@ -1,40 +1,62 @@
 package hikari.dbunit;
 
-import com.github.springtestdbunit.bean.DatabaseConfigBean;
-import com.github.springtestdbunit.bean.DatabaseDataSourceConnectionFactoryBean;
+import javax.sql.DataSource;
+
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseDataSourceConnection;
 import org.dbunit.database.IDatabaseConnection;
-import org.springframework.context.annotation.Configuration;
+import org.dbunit.ext.postgresql.PostgresqlDataTypeFactory;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
+import com.github.springtestdbunit.bean.DatabaseConfigBean;
+import com.github.springtestdbunit.bean.DatabaseDataSourceConnectionFactoryBean;
 
 /**
  * Spring Test DBUnit 用設定基底.
  */
 public abstract class AbstractSpringDbConfig {
 
-    protected static DataSource dataSource(String driverClassName, String url, String schema, String username, String password) {
+    protected static DataSource dataSource(String driverClassName, String url, String schema, String username,
+        String password) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driverClassName);
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
-        dataSource.setSchema(schema);
+        if (schema != null) {
+            dataSource.setSchema(schema);
+        }
 
         return dataSource;
     }
 
-    protected static DatabaseDataSourceConnectionFactoryBean databaseDataSourceConnectionFactoryBean(DataSource dataSource, String schema) {
-        DatabaseDataSourceConnectionFactoryBean databaseDataSourceConnectionFactoryBean = new DatabaseDataSourceConnectionFactoryBean();
+    protected static DatabaseDataSourceConnectionFactoryBean
+        databaseDataSourceConnectionFactoryBean(DataSource dataSource, String schema) {
+        DatabaseDataSourceConnectionFactoryBean databaseDataSourceConnectionFactoryBean =
+            new DatabaseDataSourceConnectionFactoryBean();
         databaseDataSourceConnectionFactoryBean.setDataSource(dataSource);
         databaseDataSourceConnectionFactoryBean.setSchema(schema);
 
-//        DatabaseConfigBean config = new DatabaseConfigBean();
-//        databaseDataSourceConnectionFactoryBean.setDatabaseConfig(config);
+        DatabaseDataSourceConnection con;
+        try {
+            con = databaseDataSourceConnectionFactoryBean.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException("オブジェクト取得失敗", e);
+        }
+
+        DatabaseConfig config = con.getConfig();
+        DatabaseConfigBean databaseConfig = new DatabaseConfigBean();
+        databaseConfig.setAllowEmptyFields(Boolean.TRUE);
+        databaseConfig.apply(config);
+
+        databaseConfig.setDatatypeFactory(new PostgresqlDataTypeFactory());
+
+        databaseDataSourceConnectionFactoryBean.setDatabaseConfig(databaseConfig);
         return databaseDataSourceConnectionFactoryBean;
     }
 
-    protected static IDatabaseConnection iDatabaseConnection(DatabaseDataSourceConnectionFactoryBean connectionFactoryBean) throws Exception {
+    protected static IDatabaseConnection
+        iDatabaseConnection(DatabaseDataSourceConnectionFactoryBean connectionFactoryBean) throws Exception {
         return connectionFactoryBean.getObject();
     }
 }
